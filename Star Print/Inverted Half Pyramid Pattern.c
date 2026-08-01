b0088d620405f6b0c347e67c6457d215f6967e7f
Md Yusuf Siyam <mdsiyam1011@gmail.com>
2026-08-01 21:59:24 +0600

    Inverted Half Pyramid Pattern

---
+++ Star Print/Inverted Half Pyramid Pattern.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+Inverted Half Pyramid Pattern
+#include <stdio.h>
+int main()
+{
+    int i, j, n;
+
+    printf("Enter height: ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+*****
+****
+***
+**
+*   
+
+*/
