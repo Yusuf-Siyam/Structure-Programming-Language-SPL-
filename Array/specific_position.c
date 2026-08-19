2f3f5936c34fe29e58955b818ae993ccef7e9ee1
Md Yusuf Siyam <mdsiyam1011@gmail.com>
2026-08-19 23:53:13 +0600

    Specific Position e data insert

---
+++ Array/specific_position.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int num[100] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int pos = 2;
+    int value = 25;
+
+    // Right shift
+    for(int i = n; i > pos; i--)
+    {
+        num[i] = num[i - 1];
+    }
+
+    // Insert value
+    num[pos] = value;
+    n++;
+
+    // Print array
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", num[i]);
+    }
+
+    return 0;
+}
+output: 10 20 25 30 40 50
