6eef9cea751cde1521b352c42cc7f1c480136364
Md Yusuf Siyam <mdsiyam1011@gmail.com>
2026-08-22 22:48:32 +0600

    main diagonal sum print

---
+++ Array-2D/Main_diagonal_sum.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[3][3], i, j, sum;
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for(i = 0; i< 3; i++)
+    {
+
+        for(j = 0; j< 3; j++)
+        if(i==j){
+            sum+=arr[i][j];
+        }
+      
+    }
+        printf("%d ",sum);
+    
+
+    return 0;
+}
