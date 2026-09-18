3a6a0166985011b96f6a8b1f557dd17eeefb9243
Md Yusuf Siyam <mdsiyam1011@gmail.com>
2026-09-18 19:19:43 +0600

    check Plaindrome or not

---
+++ String/string_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, len = 0, flag= 1;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++)
+    {
+        len++;
+    }
+
+    for (i = 0; i < len / 2; i++)
+    {
+        if (str[i] != str[len - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
